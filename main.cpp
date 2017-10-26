872622c6059206a97bc2b2d739f9252410045e91
Dylan <dylan.ritrovato@outlook.com>
2017-10-26 13:56:02 +0200

    gestion des arguments -t[0|1]

--- main.cpp
+++ main.cpp
@@ -9,8 +9,12 @@
 #include "matrice.h"
 #include <time.h>
 
-    #include "sys/times.h"
-    #include "sys/vtimes.h"
+#include "sys/times.h"
+#include "sys/vtimes.h"
+
+#define T0_MOD false
+#define T1_MOD true
+
 
 void initCPU();
 double getCurrentValue();
@@ -31,11 +35,11 @@ int main(int argc, char** argv) {
 			continue;
 		}
 		if(strcmp(argv[i], "-t0")==0){
-			mode = false;
+			mode = T0_MOD;
 			continue;
 		}
 		if(strcmp(argv[i], "-t1")==0){
-			mode = true;
+			mode = T1_MOD;
 			continue;
 		}
 		if(strcmp(argv[i], "-m")==0){
@@ -43,39 +47,50 @@ int main(int argc, char** argv) {
 			continue;
 		}
 	}
-	//création de la matrice du jeu (lignes)
-	int** matrice_jeu = static_cast<int**> (malloc(sizeof(int*)*HEIGHT));
-	if(matrice_jeu == NULL){
-		std::cerr << "ERREUR DE GENERATION DE LA MATRICE" << std::endl;
-		exit(1);
-	}
-	//création de la matrice du jeu (colonnes)
-	for (int i = 0; i < HEIGHT; i++){
-		*(matrice_jeu+i) = static_cast<int*> (malloc(sizeof(int)*WIDTH));
-		if(*(matrice_jeu+i) == NULL){
+
+	if(mode == T0_MOD){
+		//création de la matrice du jeu (lignes)
+		int** matrice_jeu = static_cast<int**> (malloc(sizeof(int*)*HEIGHT));
+		if(matrice_jeu == NULL){
 			std::cerr << "ERREUR DE GENERATION DE LA MATRICE" << std::endl;
 			exit(1);
 		}
+		//création de la matrice du jeu (colonnes)
+		for (int i = 0; i < HEIGHT; i++){
+			*(matrice_jeu+i) = static_cast<int*> (malloc(sizeof(int)*WIDTH));
+			if(*(matrice_jeu+i) == NULL){
+				std::cerr << "ERREUR DE GENERATION DE LA MATRICE" << std::endl;
+				exit(1);
+			}
+		}
+		//nombre de personnes
+		int nb = (int)pow(2, nbp);
+		//récupérer un tableau de personnes
+		Per* personnes = init(matrice_jeu,nb);
+		//récupérer un tableau de PID de threads
+		pthread_t* tid = create_threads_personnes(personnes,nb);
+		//attendre la fin des thread avant que le programme s'arrete
+		for (int i = 0; i < nb; i++)
+			pthread_join(tid[i], NULL);
+		//afficher la matrice
+	   	//affiche(matrice_jeu,HEIGHT,WIDTH);
+	   	//suppression de la memoire allouée pour ne pas avoir de fuite de memoire
+		delete[] personnes;
+		delete[] matrice_jeu;
 	}
-	//nombre de personnes
-	int nb = (int)pow(2, nbp);
-	//récupérer un tableau de personnes
-	Per* personnes = init(matrice_jeu,nb);
+	else if(mode == T1_MOD){
+		std::cout << "Fonction non implémentée..." << std::endl;
+	}
+/*
+		//AVANT
 	initCPU();
 	t1=clock();
-	//récupérer un tableau de PID de threads
-	pthread_t* tid = create_threads_personnes(personnes,nb);
-	//attendre la fin des thread avant que le programme s'arrete
-	for (int i = 0; i < nb; i++)
-       pthread_join(tid[i], NULL);
+		//PENDANT
+	
     t2=clock();
     double temps = (((double)t2)-((double)t1))/((double)CLOCKS_PER_SEC);
-    std::cout << "time: "  << (int)temps  << " Sec et " << (temps-(int)temps)*1000 << " MiliSec" << "  CPU " << getCurrentValue() << "%" << std::endl;
-   	//afficher la matrice
-   	//affiche(matrice_jeu,HEIGHT,WIDTH);
-   	//suppression de la memoire allouée pour ne pas avoir de fuite de memoire
-	delete[] personnes;
-	delete[] matrice_jeu;
+    std::cout << "time: "  << (int)temps  << " Sec et " << (temps-(int)temps)*1000 << " MiliSec" << "  CPU " << getCurrentValue() << "%" << std::endl;*/
+   	
 
 	return 0;
 }
